7a561e3666755fcc1eb0c2f24d840ee12f256aad
Roman <egrams@mail.ru>
2020-08-28 15:54:47 +0400

    Initial Commit

---
+++ DZ_1_Evtyukhin_Roman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ_1_Evtyukhin_Roman
+//
+//  Created by Роман on 28.08.2020.
+//  Copyright © 2020 Nebo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
